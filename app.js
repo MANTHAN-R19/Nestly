6c68eac4b2b2b0c5558907d7f2b63c3821b73138
Manthan <manthanbrawal@gmail.com>
2026-07-11 22:34:47 +0530

    Add home route

--- app.js
+++ app.js
@@ -68,11 +68,6 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     // res.send("Hello, I am root");
-//     res.redirect("/listings");
-// });
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -94,6 +89,10 @@ app.use((req, res, next) => {
     console.log("currUser =", res.locals.currUser);
 
     next();
+});
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
 });
 
 // app.get("/demouser", async(req, res) => {
